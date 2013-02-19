a2ca099be90b8c06b372544315d78d3820b1e34f
Kyoung-Rok <astray0924@gmail.com>
2013-02-19 15:14:03 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OpinRank::Application.config.secret_token = '95b261be23c8df7cc7a1a96c42c8ea4961593986f6412322507614ff826fcddcddad8016bd48ae16185e7ac68b7f921d30a92ed33e90e362a8cc1cc1c9d1c750'
